63794f24b223db870f475f4d274645cb93a63162
Yves Maurer <yves.maurer@bnl.etat.lu>
2014-11-28 12:03:44 +0000

    forgot to add

---
+++ namalysator/w_exportdata.h
@@ -0,0 +1,30 @@
+#ifndef W_EXPORTDATA_H
+#define W_EXPORTDATA_H
+
+#include <QtGui/QWidget>
+#include "dbrequest.h"
+
+namespace Ui
+{
+    class w_exportdata;
+}
+
+class w_exportdata : public QWidget
+{
+    Q_OBJECT
+
+public:
+    w_exportdata(QWidget *parent = 0);
+    ~w_exportdata();
+    void setBatchDetail(const BatchDetail &d);
+
+private:
+    Ui::w_exportdata *ui;
+       dbrequest db;
+    BatchDetail batchdetail;
+private slots : 
+	void exportexcel();    
+
+};
+
+#endif // W_EXPORTDATA_H
