068c39012702d4c48252dadf6fdc1966616d51df
bdebel <bdebel>
2014-12-11 13:12:57 +0000

    Remove/cleaning of old feature

--- project/altoparser.h
+++ project/altoparser.h
@@ -4,7 +4,6 @@
 #include "../common_files/xmlparser.h"
 #include "../common_files/struct.h"
 #include "../common_files/utilities.h"
-// #include "schema_validatoralto.h"
 #include "../common_files/xmltostr.h"
 #include "dataitem.h"
 #include "datafactory.h"
@@ -16,25 +15,13 @@
 class altoparserContext : public StateParserContext{
 	
 public :
-	/*
-	enum state 
-	{	
-		state_header,
-		state_alto,
-		state_block		
-	};
-	*/
 
-	//state currentstate;
-	//std::string content;	
 	std::string alto;
 	datafactory *dfalto;
-	//std::string xml;
 	std::string altoFile;
 	altoblock altoBlock;
-	//std::set<std::string> to_extract;
 	errorHandler *hError;
-	bool /*headerIn, measurement_unit,*/is_mm10/*,convert_to_pixels*/;	
+	bool is_mm10;	
 	int  dpi;
 };
 
@@ -47,7 +34,7 @@ public:
 };
 
 //! parse ALTO files and store in a datafactory
-class altoparser: public StateParserCH/*, public xmlparser_with_stack<altoparser>*/
+class altoparser: public StateParserCH
 {
 		
 private :
@@ -56,10 +43,6 @@ private :
 
 public:	
 	altoparser(const std::string &altoFile,const std::string &altoId,errorHandler *h,datafactory *df);
-	virtual void XMLstartElement(const char *name, const char **atts);
-	virtual void XMLendElement(const char *name);
-	virtual void XMLcharacterData(const char *s, int len);	
-	//virtual void getElement(const char *name, const char **atts);
 };
 
 #endif	//ALTOPARSER_H_
