80a7b10646a56e2668660e9e4122296d6535cad3
bdebel <bdebel>
2015-01-22 09:24:08 +0000

    old trace debug

--- project/datafactory.cpp
+++ project/datafactory.cpp
@@ -3,7 +3,7 @@
 
 datafactory::~datafactory()
 {
-	std::cerr <<"datafactory::~datafactory()"<<std::endl;
+	//std::cerr <<"datafactory::~datafactory()"<<std::endl;
 	for (rep_type::iterator it = item.begin(); it != item.end(); ++it) {
 		for (std::map<std::string, dataitem *>::iterator ita = it->second.begin(); ita != it->second.end(); ++ita) {
 			delete ita->second;
