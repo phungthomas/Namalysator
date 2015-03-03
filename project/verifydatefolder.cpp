d2227367dcc79d92a9c425dd44c751e6baebc3e4
bdebel <bdebel>
2015-03-03 13:27:40 +0000

    fix core dump plus behavior on error

--- project/verifydatefolder.cpp
+++ project/verifydatefolder.cpp
@@ -6,11 +6,13 @@
 verifydatefolder::verifydatefolder(errorHandler *hError,std::string &currentMetsFile, std::string &currentMetsPath)
 {
 	hError->begin("Verify: date of folder");
-	int min = 13;
-	if ( min > currentMetsPath.length() ) min = currentMetsPath.length();
-	if (currentMetsFile.substr(0,13)!=currentMetsPath.substr(currentMetsPath.length()-min,currentMetsPath.length()))
-	{			
-		hError->getError(cat_wrongFolderName,"METS","FOLDER", "Name of Metsfile : " + currentMetsFile.substr(0,13) + " != Name of folder :" + currentMetsPath.substr(currentMetsPath.length()-min,currentMetsPath.length()) ,currentMetsFile,"");	
-	
+	const int min = 13;
+	if ( min <= currentMetsPath.length() ) {
+		if (currentMetsFile.substr(0,13)!=currentMetsPath.substr(currentMetsPath.length()-min,currentMetsPath.length()))
+		{			
+			hError->getError(cat_wrongFolderName,"METS","FOLDER", "Name of Metsfile : " + currentMetsFile.substr(0,13) + " != Name of folder :" + currentMetsPath.substr(currentMetsPath.length()-min,currentMetsPath.length()) ,currentMetsFile,"");		
+		}
+	}else{
+			hError->getError(cat_wrongFolderName,"METS","FOLDER", "Name of Metsfile : " + currentMetsFile.substr(0,13) + " != Name of folder :" + currentMetsPath.substr(0,currentMetsPath.length()) ,currentMetsFile,"");			
 	}
 }
