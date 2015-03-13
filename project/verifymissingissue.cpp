1d03a7b6cc92bbbb9ea40d61b83153c5d20c4d31
Yves Maurer <yves.maurer@bnl.etat.lu>
2015-03-13 08:58:09 +0000

    corrected indenting to understand code better

--- project/verifymissingissue.cpp
+++ project/verifymissingissue.cpp
@@ -23,59 +23,47 @@ verifymissingissue::verifymissingissue(errorHandler *hError,database *db)
 			{
 				current = mets.vectIssueNumber[k];	
 				
-				if (current > 1000)
-				hError->getDateError(cat_invalidIssueNumber,"",mets.date,current+"","invalid issue number" + current);
+				if (current > 1000) {
+					hError->getDateError(cat_invalidIssueNumber,"",mets.date,current+"","invalid issue number" + current);
+				}
 		
 				findDuplicateIssue = false;				
 					
-			if (setIssue.find(current)== setIssue.end())
-			{			
-				setIssue.insert(current);
-			}
-			else if (j>0 &&  mets.date != vMets[j-1].second.date )
-			{
-				std::stringstream message;	
-				message << current ;				
-				hError->getDateError(cat_duplicateIssue,"",mets.date,message.str(),"Duplicate issue " + current);			
-				findDuplicateIssue = true;
-			} 
+				if (setIssue.find(current)== setIssue.end()) {
+					setIssue.insert(current);
+				} else if (j>0 &&  mets.date != vMets[j-1].second.date) {
+					std::stringstream message;	
+					message << current ;				
+					hError->getDateError(cat_duplicateIssue,"",mets.date,message.str(),"Duplicate issue " + current);			
+					findDuplicateIssue = true;
+				} 
 					
-			if (current-1 == before)
-			{
-										
-			}
-			else if (findDuplicateIssue ==false)				
-				if (firstItem==true)
-				{					
-					std::stringstream missingNumber;					
-					if (before +1 == current-1)
-					{
-						missingNumber << before +1;
+				if ((current-1) == before) {
+					// Correct
+				} else if (findDuplicateIssue == false) {
+					if (firstItem==true) {					
+						std::stringstream missingNumber;
+						if (before + 1 == current - 1) {
+							missingNumber << before +1;
+						} else {	
+							missingNumber << before +1 << " to " << current-1;
+						}
+						std::stringstream message;					
+						message << "Missing issue number  " << missingNumber.str() << " before " << mets.fileName;
+						std::stringstream firstdateofYear;												
+						firstdateofYear << mets.year << "-01-01";
+						hError->getDateError(cat_missingIssue,firstdateofYear.str(),mets.date,missingNumber.str(),message.str());							
+					} else if(j >0 && mets.date != vMets[j-1].second.date) {
+						std::stringstream missingNumber;					
+						if (before + 1 == current - 1) {
+							missingNumber << before +1;
+						} else {
+							missingNumber << before +1 << " to " << current-1;
+						}
+						std::stringstream message;												
+						message << "Missing issue number " << missingNumber.str() << " between " << vMets[j-1].second.date << " - " << mets.date;
+						hError->getDateError(cat_missingIssue,vMets[j-1].second.date,mets.date,missingNumber.str(),message.str());
 					}
-					else
-					{	
-						missingNumber << before +1 << " to " << current-1;
-					}
-					std::stringstream message;					
-					message << "Missing issue number  " << missingNumber.str() << " before " << mets.fileName;
-					std::stringstream firstdateofYear;												
-					firstdateofYear << mets.year << "-01-01";
-					hError->getDateError(cat_missingIssue,firstdateofYear.str(),mets.date,missingNumber.str(),message.str());							
-				}
-				else if(j >0 && mets.date != vMets[j-1].second.date)						
-				{	
-					std::stringstream missingNumber;					
-					if (before +1 == current-1)
-					{
-						missingNumber << before +1;
-					}
-					else
-					{	
-						missingNumber << before +1 << " to " << current-1;
-					}
-					std::stringstream message;												
-					message << "Missing issue number " << missingNumber.str() << " between " << vMets[j-1].second.date << " - " << mets.date;
-					hError->getDateError(cat_missingIssue,vMets[j-1].second.date,mets.date,missingNumber.str(),message.str());
 				}
 				before = current;	
 				firstItem = false;			
