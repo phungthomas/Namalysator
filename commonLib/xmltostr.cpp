c7715091fe6f0b725c21a8910ec0e1cdc6d047a5
bdebel <bdebel>
2015-03-16 15:33:33 +0000

    old code

--- commonLib/xmltostr.cpp
+++ commonLib/xmltostr.cpp
@@ -68,67 +68,7 @@ std::string xml2str(const char *src, int len)
 	}
 }
 
-/*
-std::string xml2stringHeader(const char *name, const char **atts)
-{
-	std::string xml;
-	
-	if (strcmp(name,"MODS:mods")==0)
-	{
-		xml +="<MODS:mods xmlns:MODS=\"http://www.loc.gov/mods/v3\" xmlns:xlink=\"http://www.w3.org/TR/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/mods/v3 \
- " + linkSchemas +"\\mods-3-0.xsd\">";
-	}
-	else if (strcmp(name,"mix:mix")==0)
-	{
-		xml +="<mix:mix xmlns:mix=\"http://www.loc.gov/mix/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/mix/ \
-" + linkSchemas +"\\mix.xsd\">";
-	}
-	else if (strcmp(name,"alto")==0)
-	{	
-		xml += "<alto xmlns=\"http://schema.ccs-gmbh.com/ALTO\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://schema.ccs-gmbh.com/ALTO " + linkSchemas +"\\alto-1-2.xsd\">";
-	}
-	return xml;
 
-}
-
-std::string xml2stringBegin(const char *name, const char **atts)
-{	
-	std::string xml;
-	xml += "<";
-	xml += name;
-	if (atts)
-	{
-		while (*atts)
-		{
-			xml += " ";
-			xml += *atts;				
-			xml += "=\"" + xml2str(*(atts + 1),0) + "\"";			
-			atts += 2;
-		}
-	}
-		xml += ">";
-	
-	return xml;
-}
-
-std::string xml2stringEnd(const char *name)
-{
-	std::string xml;
-			
-	xml += "</";
-	xml += name;
-	xml += ">";
-
-	return xml;
-}
-
-std::string initializeXmlValidator()
-{	
-	std::string xml = "<?xml version=\"1.0\"  encoding=\"UTF-8\" ?>";
-	return xml;
-}
-
-*/
 
 std::string xml2stringData(const char *s, int len)
 {
